305db3e44bbcbebba4fbcd6612d8285b295c9b9b
Luan Jodar <luan.jodar@gmaiil.com>
2015-05-26 18:51:54 -0300

    Primeiro commit do sistema de restaurante do Livro da Caelum

---
+++ app/models/restaurante.rb
@@ -0,0 +1,17 @@
+class Restaurante < ActiveRecord::Base
+	validates_presence_of :nome, message: "Deve ser preenchido!"
+	validates_presence_of :endereco, message: "Deve ser preenchido!"
+	validates_presence_of :especialidade, message: "Deve ser preenchido!"
+
+	validates_uniqueness_of :nome, message: "Nome já existente!"
+	validates_uniqueness_of :endereco, message: "Endereço já existente!"
+
+	validate :primeira_letra_deve_ser_maiuscula
+
+	private
+	def primeira_letra_deve_ser_maiuscula
+		errors.add(:nome,
+			"A primeira letra deve ser maiuscula!") unless nome=~ /[A-Z].*/
+			end
+	end
+end
